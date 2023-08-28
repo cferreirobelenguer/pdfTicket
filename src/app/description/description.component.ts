70924c107af12e44a88580f98269f5c236a10f9e
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-28 18:55:23 +0200

    pdf

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './service/product.service';
 import { Product } from './producto';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+
+(pdfMake as any).vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-description',
@@ -36,7 +40,50 @@ export class DescriptionComponent implements OnInit{
   }
 
   //pdf function
-  public handleClick() : void {
-
+  public handleClick() : void { 
+    const IVA = Math.round(this.data.price * 0.24);
+    const totalPrice = Math.round(this.data.price - IVA);
+ 
+    const pdfDefinition: any = {
+      content: [
+        {
+          table: {
+            widths: ['auto', 'auto'], 
+            body: [
+              [
+                {
+                  text: 'Artículo: ' + this.data.title,
+                  style: 'cell',
+                },
+                {
+                  text: 'IVA: ' + IVA.toFixed(2) + '€',
+                  style: 'cell',
+                },
+              ],
+              [
+                {
+                  text: 'Precio: ' + this.data.price + '€',
+                  style: 'cell',
+                },
+                {
+                  text: 'Total a pagar: ' + totalPrice.toFixed(2) + '€',
+                  style: 'cell',
+                },
+              ],
+            ],
+          },
+        },
+      ],
+      styles: {
+        cell: {
+          fontSize: 16,
+          padding: 5,
+        },
+      },
+      layout: 'noBorders', 
+    };
+  
+    const pdf = pdfMake.createPdf(pdfDefinition);
+    pdf.open();
   }
 }
